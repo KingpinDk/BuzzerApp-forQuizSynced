f9a97c4783ae008941f9014a9a2df3a60c2b9d1c
dk <simplykingpin@gmail.com>
2023-01-29 10:14:59 +0530

    mistaken

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,13 @@ class _HomeState extends State<Home> {
                 ),
               ),
               ListTile(
+                title: const Text('Home'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
                 title: const Text('Times'),
                 onTap: () {
                   // Update the state of the app.
@@ -88,6 +95,7 @@ class _HomeState extends State<Home> {
         ),
       ),
     );
+    lets make mistank
   }
 }
 
